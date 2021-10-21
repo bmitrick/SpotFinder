3713879e14648084a937275f069dd6cf1ff9b61e
Blake Mitrick <bmitrick@gmail.com>
2021-10-20 22:24:55 -0400

    got web working, kind of

--- src/js/app.js
+++ src/js/app.js
@@ -3,16 +3,17 @@ App = {
   contracts: {},
 
   init: async function() {
-    // Load pets.
+    // Load spots.
     $.getJSON('../spots.json', function(data) {
-      var spotsRow = $('#spotRow');
+      var spotsRow = $('#spotsRow');
       var spotTemplate = $('#spotTemplate');
 
       for (i = 0; i < data.length; i ++) {
-        spotTemplate.find('.panel-title').text(data[i].lotName);
+        spotTemplate.find('.panel-title').text(data[i].name);
         spotTemplate.find('img').attr('src', data[i].picture);
+        spotTemplate.find('.availability').text(data[i].availability);
         spotTemplate.find('.spot-price').text(data[i].price);
-        spotTemplate.find('.spot-address').text(data[i].address);
+        spotTemplate.find('.spot-location').text(data[i].location);
         spotTemplate.find('.btn-rent').attr('data-id', data[i].id);
 
         spotsRow.append(spotTemplate.html());
@@ -23,45 +24,101 @@ App = {
   },
 
   initWeb3: async function() {
-    /*
-     * Replace me...
-     */
+    if (window.ethereum) {
+      App.web3Provider = window.ethereum;
+      try {
+        // Request account access
+        await window.ethereum.enable();
+      } catch (error) {
+        // User denied account access...
+        console.error("User denied account access")
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      App.web3Provider = window.web3.currentProvider;
+    }
+    // If no injected web3 instance is detected, fall back to Ganache
+    else {
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+    web3 = new Web3(App.web3Provider);
+
+    return App.initContract();
 
     return App.initContract();
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
+    $.getJSON('Rent.json', function(data) {
+      // Get the necessary contract artifact file and instantiate it with @truffle/contract
+      var RentalArtifact = data;
+      App.contracts.Rent = TruffleContract(RentalArtifact);
+
+      // Set the provider for our contract
+      App.contracts.Rent.setProvider(App.web3Provider);
+
+      // Use our contract to retrieve and mark the adopted pets
+      return App.markRented();
+    });
 
     return App.bindEvents();
   },
 
   bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
+    $(document).on('click', '.btn-rent', App.handleRented);
   },
 
-  markAdopted: function() {
-    /*
-     * Replace me...
-     */
+  markRented: function() {
+    var rentalInstance;
+
+    App.contracts.Rent.deployed().then(function(instance) {
+      rentalInstance = instance;
+
+      return rentalInstance.getDrivers.call();
+    }).then(function(renters) {
+      for (i = 0; i < renters.length; i++) {
+        if (renters[i] !== '0x0000000000000000000000000000000000000000') {
+          $('.panel-spot').eq(i).find('button').text('Success').attr('disabled', true);
+        }
+      }
+    }).catch(function(err) {
+      console.log(err.message);
+    });
   },
 
-  handleAdopt: function(event) {
+  handleRented: function(event) {
     event.preventDefault();
 
-    var petId = parseInt($(event.target).data('id'));
+    var spotId = parseInt($(event.target).data('id'));
 
-    /*
-     * Replace me...
-     */
+    var rentalInstance;
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.Rent.deployed().then(function(instance) {
+        rentalInstance = instance;
+
+        // Execute adopt as a transaction by sending account
+        return rentalInstance.rentSpot(spotId, {from: account});
+      }).then(function(result) {
+        return App.markRented();
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
   }
 
 };
 
 $(function() {
-  $(window).load(function() {
+  $(document).ready(function() {
     App.init();
+    console.log("test");
   });
 });
