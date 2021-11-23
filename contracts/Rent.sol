85787f22aaa4e84bb0f2be41f3052107034c271c
Blake Mitrick <bmitrick@gmail.com>
2021-11-23 15:56:24 -0500

    got login redirect working

--- contracts/Rent.sol
+++ contracts/Rent.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.5.0;
 
 contract Rent {
-  address[16] public drivers;
+  address[100] public drivers;
 
   //Renting a spot
   function rentSpot(uint spotId) public returns (uint) {
-    require (spotId >= 0 && spotId <= 15);
+    require (spotId >= 0 && spotId <= 99);
 
     drivers[spotId] = msg.sender;
 
@@ -13,7 +13,7 @@ contract Rent {
   }
 
   //Retrieving the drivers
-  function getDrivers() public view returns (address[16] memory) {
+  function getDrivers() public view returns (address[100] memory) {
     return drivers;
   }
 }
